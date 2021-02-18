58ce00bf7cfadbb6f2728b0383049ed3ab7e46d5
adrenalize-brad <brad@adrenalizedigital.ca>
2021-02-18 14:37:54 -0700

    update blog post template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,105 +2,198 @@ import React from 'react'
 import { graphql, Link } from 'gatsby'
 import Img from 'gatsby-image'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import ChevronLeftRoundedIcon from '@material-ui/icons/ChevronLeftRounded';
+import ChevronRightRoundedIcon from '@material-ui/icons/ChevronRightRounded';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import PinterestIcon from '@material-ui/icons/Pinterest';
+import RedditIcon from '@material-ui/icons/Reddit';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import EmailIcon from '@material-ui/icons/Email';
+import { FacebookShareButton, FacebookMessengerShareButton, LinkedinShareButton, TwitterShareButton, PinterestShareButton, RedditShareButton, WhatsappShareButton, EmailShareButton } from 'react-share'
+import Footer from '../components/footer'
 
 function BlogPostTemplate({
   data: { authorImage, coverImage },
   pageContext: { nextPost, page, previousPost },
 }) {
+
   return (
-    <article>
-      <header className="pt-6 lg:pb-10">
-        <div className="space-y-1 text-center">
-          <dl className="space-y-10">
-            <div>
-              <dt className="sr-only">Published on</dt>
-              <dd className="text-base leading-6 font-medium text-gray-500">
-                <time dateTime={page.date}>{page.date}</time>
-              </dd>
+    <div>
+
+    <article className="post divide-y divide-gray-500">
+      
+      <header className="header">
+
+        <h1>{page.title}</h1>
+
+        <div className="sub-header pb-2 pb-5">
+
+          <div className="author pt-1">
+
+            <div className="avatar">
+              <Img fluid={authorImage.localFile.childImageSharp.fluid} className="rounded-full" fadeIn={false} />
             </div>
-          </dl>
-          <div>
-            <h1 className="text-3xl leading-9 font-extrabold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-              {page.title}
-            </h1>
+
+            <div className="profile flex-grow">
+
+              <h5>{page.author.name}</h5>
+
+              {page.author.title && (
+                  <React.Fragment>    
+                    <p>{page.author.title}</p>
+                  </React.Fragment>
+              )}
+                          
+            </div>
+                
           </div>
-        </div>
+
+
+          <div className="time">
+            <time dateTime={page.date}><p>{page.date}</p></time>
+          </div>   
+
+        </div>         
+
       </header>
-      <div
-        className="divide-y lg:divide-y-0 divide-gray-200 lg:grid lg:grid-cols-4 lg:col-gap-6 pb-16 lg:pb-20"
-        style={{ gridTemplateRows: 'auto 1fr' }}
-      >
-        <dl className="pt-6 pb-10 lg:pt-11 lg:border-b lg:border-gray-200">
-          <dt className="sr-only">Author</dt>
-          <dd>
-            <ul className="flex justify-center lg:block space-x-8 sm:space-x-12 lg:space-x-0 lg:space-y-8">
-              <li className="flex space-x-2">
-                <Img
-                  fluid={authorImage.localFile.childImageSharp.fluid}
-                  className="w-10 h-10 rounded-full"
-                  fadeIn={false}
-                />
-                <dl className="flex-1 text-sm font-medium leading-5">
-                  <dt className="sr-only">Name</dt>
-                  <dd className="text-gray-900">{page.author.name}</dd>
-                  {page.author.title && (
-                    <React.Fragment>
-                      <dt className="sr-only">Title</dt>
-                      <dd className="text-gray-500">{page.author.title}</dd>
-                    </React.Fragment>
-                  )}
-                </dl>
-              </li>
-            </ul>
-          </dd>
-        </dl>
-        <div className="divide-y divide-gray-200 lg:pb-0 lg:col-span-3 lg:row-span-2">
-          {coverImage && (
-            <Img
-              fluid={coverImage.localFile.childImageSharp.fluid}
-              className="mb-8 rounded"
-              fadeIn={false}
-            />
-          )}
-          <div className="prose max-w-none pt-10 pb-8">
-            <MDXRenderer>{page.content.markdownNode.childMdx.body}</MDXRenderer>
-          </div>
+
+      <div className="content py-5">
+
+        <div className="excerpt pb-5 mx-3">
+          <p>{page.excerpt}</p>
         </div>
-        <footer className="text-sm font-medium leading-5 divide-y divide-gray-200 lg:col-start-1 lg:row-start-2">
-          {(nextPost || previousPost) && (
-            <div className="space-y-8 py-8">
-              {nextPost && (
-                <div>
-                  <h2 className="text-xs tracking-wide uppercase text-gray-500">
-                    Next Post
-                  </h2>
-                  <div className="text-purple-500 hover:text-purple-600">
-                    <Link to={`/posts/${nextPost.slug}`}>{nextPost.title}</Link>
-                  </div>
-                </div>
-              )}
-              {previousPost && (
-                <div>
-                  <h2 className="text-xs tracking-wide uppercase text-gray-500">
-                    Previous Post
-                  </h2>
-                  <div className="text-purple-500 hover:text-purple-600">
-                    <Link to={`/posts/${previousPost.slug}`}>
-                      {previousPost.title}
-                    </Link>
-                  </div>
-                </div>
-              )}
-            </div>
-          )}
-          <div className="pt-8">
-            <Link to="/" className="text-purple-500 hover:text-purple-600">
-              &larr; Back to the blog
-            </Link>
-          </div>
-        </footer>
+
+        <div className="image">
+          {coverImage && (
+              <Img
+                fluid={coverImage.localFile.childImageSharp.fluid}
+                className="post-image"
+                fadeIn={false}
+              />
+            )}
+        </div>
+
+        <div className="body pt-6 pb-2 mx-3">
+          <MDXRenderer>{page.content.markdownNode.childMdx.body}</MDXRenderer>
+        </div>
+
+        
+
       </div>
+
+      
+
+      <footer>
+
+        {(nextPost || previousPost) && (
+
+          <div className="footer-post-nav pt-2">
+
+             {previousPost && (
+
+                <Link to={`/posts/${previousPost.slug}`}>
+
+                  <div className="previous text-left py-3">
+                    
+                    <div className="row"> 
+                      <ChevronLeftRoundedIcon/><h3>Previous Post</h3>
+                    </div>
+
+                    <div className="link ml-6">
+                      <h5>{previousPost.title}</h5>
+                    </div>
+                      
+                  </div>
+
+                </Link>
+
+              )}
+
+<div className="social-sharing text-center">
+
+  <div className="wrapper">
+
+<p>Be Cool. Share with your friends.</p>
+
+  <FacebookShareButton 
+    url={`/posts/${page.slug}`}
+    quote={page.excerpt}
+    hashtag={`#${page.title}`}
+  ><FacebookIcon/></FacebookShareButton>
+
+  <FacebookMessengerShareButton
+    url={`/posts/${page.slug}`}
+  ><i className="fab fa-facebook-messenger"/></FacebookMessengerShareButton>
+
+  <LinkedinShareButton 
+    url={`/posts/${page.slug}`}
+    title={page.title}
+    summary={page.excerpt}
+    source="https://www.adrenalizedigital.ca"
+  ><LinkedInIcon/></LinkedinShareButton>
+
+  <TwitterShareButton 
+    url={`/posts/${page.slug}`}
+    title={page.title}
+    hashtags={["adrenalizeDigital,webDev"]}
+  ><TwitterIcon/></TwitterShareButton>
+
+  <PinterestShareButton 
+    url={`/posts/${page.slug}`}
+    media={coverImage.localFile}
+    description={page.excerpt}
+  ><PinterestIcon/></PinterestShareButton>
+
+  <RedditShareButton 
+    url={`/posts/${page.slug}`}
+    title={page.title}
+  ><RedditIcon/></RedditShareButton>
+
+  <WhatsappShareButton 
+    url={`/posts/${page.slug}`}
+    title={page.title}
+  ><WhatsAppIcon/></WhatsappShareButton>
+
+  <EmailShareButton 
+    url={`https://www.adrenalizedigital.ca/posts/${page.slug}`}
+    subject={page.title}
+    body={page.excerpt}
+  ><EmailIcon/></EmailShareButton>
+</div>
+</div>
+
+
+              {nextPost && (
+                <Link to={`/posts/${nextPost.slug}`}>
+
+                  <div className="next text-right py-3">
+
+                    <div className="row"> 
+                      <h3>Next Post</h3><ChevronRightRoundedIcon/>
+                    </div>
+
+                    <div className="link mr-6">
+                      <h5>{nextPost.title}</h5>         
+                    </div>
+
+                  </div>
+
+                </Link>  
+
+              )}
+
+          </div>
+        )}
+    
+        </footer>
+
     </article>
+
+    <Footer/>
+
+    </div>
   )
 }
 
@@ -133,3 +226,4 @@ export const pageQuery = graphql`
 `
 
 export default BlogPostTemplate
+
